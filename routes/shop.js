3e68aeac4a49bdb315b0eed8629833fe8ed27625
Mikaela-D <G00401392@atu.ie>
2025-04-06 22:16:46 +0100

    Added promises

--- routes/shop.js
+++ routes/shop.js
@@ -23,62 +23,64 @@ const productSchema = new Schema({
 const Product = mongoose.model("product", productSchema);
 
 const generateUniqueId = async () => {
-  const products = await Product.find().sort({ ourId: 1 });
-  let newId = 1;
-  for (const product of products) {
-    if (parseInt(product.ourId) !== newId) {
-      break;
+  try {
+    const products = await Product.find().sort({ ourId: 1 });
+    let newId = 1;
+    for (const product of products) {
+      if (parseInt(product.ourId) !== newId) {
+        break;
+      }
+      newId++;
     }
-    newId++;
+    return newId.toString();
+  } catch (err) {
+    console.log("Error generating unique ID:", err);
+    throw err;
   }
-  return newId.toString();
 };
 
 router.get("/addProduct", async (req, res, next) => {
-  const newId = await generateUniqueId();
-  new Product({
-    ourId: newId,
-    name: "widget",
-    price: 3.95,
-    category: "gadgets",
-    brand: "widgetBrand",
-    description: "A useful widget",
-    color: "red",
-    weight: "200g",
-    availability: "In stock",
-    size: "large",
-  })
-    .save()
-    .then((result) => {
-      console.log("saved product to database");
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log("failed to addAproduct: " + err);
-      res.redirect("/");
+  try {
+    const newId = await generateUniqueId();
+    const product = new Product({
+      ourId: newId,
+      name: "widget",
+      price: 3.95,
+      category: "gadgets",
+      brand: "widgetBrand",
+      description: "A useful widget",
+      color: "red",
+      weight: "200g",
+      availability: "In stock",
+      size: "large",
     });
+    await product.save();
+    console.log("Saved product to database");
+    res.redirect("/");
+  } catch (err) {
+    console.log("Failed to add product:", err);
+    res.redirect("/");
+  }
 });
 
 router.get("/", (req, res, next) => {
-  console.log(req.query);
   Product.find()
     .then((products) => {
       res.send(JSON.stringify(products));
     })
     .catch((err) => {
-      console.log("Failed to find: " + err);
+      console.log("Failed to find products:", err);
       res.send(JSON.stringify(err));
     });
 });
 
 router.post("/", (req, res, next) => {
-  console.log(req.body);
   Product.find()
     .then((products) => {
       res.json({ success: true, Products: products });
     })
     .catch((err) => {
-      console.log("Failed to find: " + err);
+      console.log("Failed to find products:", err);
       res.json({ success: false, theError: err });
     });
 });
@@ -89,7 +91,7 @@ router.get("/getSpecificProduct", (req, res, next) => {
       res.send(JSON.stringify(products[0]));
     })
     .catch((err) => {
-      console.log("Failed to find product: " + err);
+      console.log("Failed to find product:", err);
       res.send(JSON.stringify(err));
     });
 });
@@ -100,92 +102,98 @@ router.post("/getSpecificProduct", (req, res, next) => {
       res.json({ success: true, theProduct: products[0] });
     })
     .catch((err) => {
-      console.log("Failed to find product: " + err);
+      console.log("Failed to find product:", err);
       res.json({ success: false, theError: err });
     });
 });
 
 router.post("/addProduct", async (req, res, next) => {
-  const {
-    name,
-    price,
-    category,
-    brand,
-    description,
-    color,
-    weight,
-    availability,
-  } = req.body;
-  const newId = await generateUniqueId();
-  new Product({
-    ourId: newId,
-    name,
-    price,
-    category,
-    brand,
-    description,
-    color,
-    weight,
-    availability,
-  })
-    .save()
-    .then((result) => {
-      console.log("saved product to database");
-      res.json({ success: true });
-    })
-    .catch((err) => {
-      console.log("failed to add product: " + err);
-      res.json({ success: false, theError: err });
+  try {
+    const {
+      name,
+      price,
+      category,
+      brand,
+      description,
+      color,
+      weight,
+      availability,
+    } = req.body;
+    const newId = await generateUniqueId();
+    const product = new Product({
+      ourId: newId,
+      name,
+      price,
+      category,
+      brand,
+      description,
+      color,
+      weight,
+      availability,
     });
+    await product.save();
+    console.log("Saved product to database");
+    res.json({ success: true });
+  } catch (err) {
+    console.log("Failed to add product:", err);
+    res.json({ success: false, theError: err });
+  }
 });
 
-router.post("/updateSpecificProduct", (req, res, next) => {
-  const {
-    ourId,
-    name,
-    price,
-    category,
-    brand,
-    description,
-    color,
-    weight,
-    availability,
-  } = req.body;
-  console.log("Updating product with ID:", ourId);
-  Product.findOneAndUpdate(
-    { ourId },
-    { name, price, category, brand, description, color, weight, availability },
-    { new: true }
-  )
-    .then((updatedProduct) => {
-      if (!updatedProduct) {
-        console.log("Product not found");
-        return res.json({ success: false, theError: "Product not found" });
-      }
-      console.log("Updated product in database:", updatedProduct);
-      res.json({ success: true, updatedProduct });
-    })
-    .catch((err) => {
-      console.log("Failed to update product:", err);
-      res.json({ success: false, theError: err });
-    });
+router.post("/updateSpecificProduct", async (req, res, next) => {
+  try {
+    const {
+      ourId,
+      name,
+      price,
+      category,
+      brand,
+      description,
+      color,
+      weight,
+      availability,
+    } = req.body;
+    console.log("Updating product with ID:", ourId);
+    const updatedProduct = await Product.findOneAndUpdate(
+      { ourId },
+      {
+        name,
+        price,
+        category,
+        brand,
+        description,
+        color,
+        weight,
+        availability,
+      },
+      { new: true }
+    );
+    if (!updatedProduct) {
+      console.log("Product not found");
+      return res.json({ success: false, theError: "Product not found" });
+    }
+    console.log("Updated product in database:", updatedProduct);
+    res.json({ success: true, updatedProduct });
+  } catch (err) {
+    console.log("Failed to update product:", err);
+    res.json({ success: false, theError: err });
+  }
 });
 
-router.post("/deleteSpecificProduct", (req, res, next) => {
-  const { ourId } = req.body;
-  Product.findOneAndRemove({ ourId })
-    .then((deletedProduct) => {
-      if (!deletedProduct) {
-        console.log("Product not found");
-        return res.json({ success: false, theError: "Product not found" });
-      }
-      console.log("Deleted product from database:", deletedProduct);
-      res.json({ success: true });
-    })
-    .catch((err) => {
-      console.log("Failed to delete product:", err);
-      res.json({ success: false, theError: err });
-    });
+router.post("/deleteSpecificProduct", async (req, res, next) => {
+  try {
+    const { ourId } = req.body;
+    const deletedProduct = await Product.findOneAndRemove({ ourId });
+    if (!deletedProduct) {
+      console.log("Product not found");
+      return res.json({ success: false, theError: "Product not found" });
+    }
+    console.log("Deleted product from database:", deletedProduct);
+    res.json({ success: true });
+  } catch (err) {
+    console.log("Failed to delete product:", err);
+    res.json({ success: false, theError: err });
+  }
 });
 
 exports.routes = router;
